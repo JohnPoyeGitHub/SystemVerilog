b009efd5b8655ac760b7a542357fb7083c77e3bf
JohnPoyleGithub <123499266+JohnPoyeGitHub@users.noreply.github.com>
2024-09-12 16:16:33 +0200

    Add files via upload

---
+++ nand2_delay.sv
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+
+module nand2_delay ( 
+	input logic a,   // First input 
+	input logic b,   // Second input 
+	output logic y   // Output 
+	); 
+
+// Continuous assignment for NAND gate 
+    assign #1ns y = ~(a & b);  // Perform the NAND operation after the delay
+	
+endmodule
