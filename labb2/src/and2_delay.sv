1782f0927f3cfd05e7969f6312410f5114270c66
JohnPoyleGithub <123499266+JohnPoyeGitHub@users.noreply.github.com>
2024-10-01 17:41:39 +0000

    commit changes

--- labb2/src/and2_delay.sv
+++ labb2/src/and2_delay.sv
@@ -5,15 +5,6 @@ module nand2_delay(
 );
     logic c;
 
-    nand2_delay u1(
-        .a(a),
-        .b(b),
-        .y(c)
-    );
-    nand2_delay u2(
-        .a(c),
-        .b(c),
-        .y(prod_terms)
-    );
+    prod_terms = a&b;
 
 endmodule
